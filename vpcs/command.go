6eaf65f9b8b24ae5ecf797919d390dfeb32ea2eb
hirakiuc <hirakiuc@gmail.com>
2015-06-21 13:49:34 +0900

    implement vpcs command

--- vpcs/command.go
+++ vpcs/command.go
@@ -17,5 +17,24 @@ func (c *Command) Help() string {
 }
 
 func (c *Command) Run(args []string) int {
+	c.parseOptions(args)
+	c.showVpcs(os.Stdout)
 	return 0
+}
+
+func (c *Command) Synopsis() string {
+	return "Show vpcs."
+}
+
+func (c *Command) parseOptions(args []string) {
+	var configPath string
+	f := flag.NewFlagSet("vpcs", flag.ExitOnError)
+	f.StringVar(&configPath, "c", "~/.ec2s.toml", "config path")
+	f.Parse(args)
+
+	_, err := config.LoadConfig(configPath)
+	if err != nil {
+		fmt.Printf("Can't load config file: %s, %v\n", configPath, err)
+		os.Exit(1)
+	}
 }
--- vpcs/logic.go
+++ vpcs/logic.go
@@ -4,6 +4,7 @@ import (
 	"../config"
 	"fmt"
 	"io"
+	"os"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
@@ -12,19 +13,68 @@ import (
 	"github.com/olekukonko/tablewriter"
 )
 
-func (c *Command) showList(writer io.Writer) {
+func showError(err error) {
+	if awsErr, ok := err.(awserr.Error); ok {
+		// Generic AWS Error with Code, Message, and original error (if any)
+		fmt.Println(awsErr.Code(), awsErr.Message(), awsErr.OrigErr())
+		if reqErr, ok := err.(awserr.RequestFailure); ok {
+			// A service error occurred
+			fmt.Println(reqErr.Code(), reqErr.Message(), reqErr.StatusCode(), reqErr.RequestID())
+		}
+	} else {
+		// This case should never be hit, the SDK should always return an error
+		// which satisfies the awserr.Error interface.
+		fmt.Println(err.Error())
+	}
+}
+
+func describeVpc(writer *tablewriter.Table, v *ec2.VPC) {
+	var tag_name string
+	for _, t := range v.Tags {
+		if *t.Key == "Name" {
+			tag_name = *t.Value
+		}
+	}
+
+	writer.Append([]string{
+		tag_name,
+		*v.VPCID,
+		*v.State,
+	})
+}
+
+func (c *Command) showVpcs(writer io.Writer) {
 	conf := config.GetConfig()
 
 	credentials := credentials.NewStaticCredentials(
 		conf.Aws.AccessKeyId,
 		conf.Aws.SecretAccessKey,
-		""
+		"",
 	)
 
 	svc := ec2.New(
 		&aws.Config{
-			Region: conf.Aws.Region,
+			Region:      conf.Aws.Region,
 			Credentials: credentials,
 		},
 	)
+
+	res, err := svc.DescribeVPCs(nil)
+	if err != nil {
+		fmt.Println("failed...")
+		showError(err)
+		os.Exit(1)
+	}
+
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetBorder(false)
+	table.SetRowLine(false)
+	table.SetColumnSeparator("\t")
+	table.SetColWidth(80)
+
+	for _, vpc := range res.VPCs {
+		describeVpc(table, vpc)
+	}
+
+	table.Render()
 }
