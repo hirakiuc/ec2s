841b87ffc4d58b32b5d31b7a83f3ad442fd9e14d
hirakiuc <hirakiuc@gmail.com>
2016-05-29 14:28:42 +0900

    add ArgumentError. remove TODO comment.

--- internal/common/error.go
+++ internal/common/error.go
@@ -4,12 +4,29 @@ import (
 	"fmt"
 )
 
+// ArgumentError describe a error that receiver passed invalid arguments.
+type ArgumentError struct {
+	Cause string
+}
+
+// NewArgumentError create ArgumentError instance.
+func NewArgumentError(msg string) ArgumentError {
+	return ArgumentError{
+		Cause: msg,
+	}
+}
+
+// Error return error message
+func (err ArgumentError) Error() string {
+	return fmt.Sprintf("Argument Error: %s", err.Cause)
+}
+
 // VpcNotFoundError describe a error that  Vpc not found.
 type VpcNotFoundError struct {
 	Cause string
 }
 
 // Error return error message
-func (err *VpcNotFoundError) Error() string {
+func (err VpcNotFoundError) Error() string {
 	return fmt.Sprintf("No such vpc found. %s", err.Cause)
 }
